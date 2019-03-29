65de8f9af9a97fdda714443c5319a680063f4ab6
huenisys <paul@huenits.com>
2019-03-29 13:21:48 +0800

    changed to unit test

--- tests/Unit/MdFileParserTest.php
+++ tests/Unit/MdFileParserTest.php
@@ -1,16 +1,17 @@
 <?php
 
-namespace huenisys\Blog\Tests\Feature;
+namespace huenisys\Publisher\Tests;
 
 use Orchestra\Testbench\TestCase;
-use huenisys\Blog\MdBlogParser;
+use huenisys\Publisher\MdFileParser;
 
-class MdBlogParserTest extends TestCase
+class MdFileParserTest extends TestCase
 {
     /** @test **/
     public function meta_and_body_gets_split()
     {
-        $parsedPost1 = (new MdBlogParser(__DIR__.'/../blog-files-in-md/Post1.md'));
+        $parsedPost1 = (new MdFileParser(__DIR__.'/../files-in-md/Post1.md'));
+        dump($parsedPost1);
         $this->assertStringContainsString('title: Some Title', $parsedPost1->getMetaInYaml());
         $this->assertStringContainsString('This is the body', $parsedPost1->getBodyInMd());
     }
@@ -18,14 +19,14 @@ class MdBlogParserTest extends TestCase
     /** @test **/
     public function get_the_meta_title()
     {
-       $parsedPost1 = (new MdBlogParser(__DIR__.'/../blog-files-in-md/Post1.md'));
+       $parsedPost1 = (new MdFileParser(__DIR__.'/../files-in-md/Post1.md'));
        $this->assertEquals('Some Title', $parsedPost1->getMeta('title'));
     }
 
     /** @test **/
     public function another_meta_props_check()
     {
-       $parsedPost2 = (new MdBlogParser(__DIR__.'/../blog-files-in-md/Post2.md'));
+       $parsedPost2 = (new MdFileParser(__DIR__.'/../files-in-md/Post2.md'));
        $this->assertEquals('Post2 Title', $parsedPost2->getMeta('title'));
        $this->assertEquals('Garfield', $parsedPost2->getMeta('author'));
 
@@ -39,8 +40,16 @@ class MdBlogParserTest extends TestCase
     /** @test **/
     public function allow_no_meta()
     {
-       $parsedPost3 = (new MdBlogParser(__DIR__.'/../blog-files-in-md/Post3-with-no-meta.md'));
+       $parsedPost3 = (new MdFileParser(__DIR__.'/../files-in-md/Post3-with-no-meta.md'));
        $this->assertEmpty($parsedPost3->getMeta());
        $this->assertStringContainsString('Post with no met', $parsedPost3->getBodyInMd());
+    }
+
+    /** @test **/
+    public function parse_md_content_only()
+    {
+       $parsedMdContent = (new MdFileParser('# md content only'));
+       $this->assertEquals($parsedMdContent->getBodyInMd(), '# md content only');
+       $this->assertEquals($parsedMdContent->getBodyInHtml(), '<h1>md content only</h1>');
     }
 }
